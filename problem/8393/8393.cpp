45cbef5a8b5e8eac995af4ea9327119a770cc0db
DC-template <jeonghyeon.lee1108@gmail.com>
2017-03-29 20:43:05 +0900

    https://www.acmicpc.net/problem/8393

---
+++ problem/8393/8393.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int main() {
+    int n;
+    int sum=0;
+
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++) {
+        sum += i;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
