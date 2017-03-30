5cd1015e616c71ad0a1cabf2659e0777e119f5b1
jhlee1108 <jeonghyeon.lee1108@gmail.com>
2017-03-31 01:05:04 +0900

    https://www.acmicpc.net/problem/2522

---
+++ problem/2522/2522.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++) {
+        for(int j=i; j<n; j++) {
+            printf(" ");
+        }
+
+        for(int j=0; j<i; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    for(int i=1; i<=n-1; i++) {
+        for(int j=0; j<i; j++) {
+            printf(" ");
+        }
+
+        for(int j=i; j<n; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
