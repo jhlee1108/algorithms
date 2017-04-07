9ee3b707243afb6cb9731eb3d041e97166002a26
jhlee1108 <jeonghyeon.lee1108@gmail.com>
2017-04-07 17:29:44 +0900

    https://www.acmicpc.net/problem/1152

---
+++ problem/1152/1152.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string s;
+    int count = 0;
+
+    while(cin >> s) {
+        count++;
+    }
+
+    cout << count;
+
+    return 0;
+}
