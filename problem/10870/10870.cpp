653fd0f517683ddffdbbf7f168c23da83a852961
jhlee1108 <jeonghyeon.lee1108@gmail.com>
2017-04-17 23:40:05 +0900

    https://www.acmicpc.net/problem/10870

---
+++ problem/10870/10870.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <functional>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    function<int(int)> f = [&](int n) {
+        if(n <=1 )
+            return n;
+        else
+            return f(n-1) + f(n-2);
+    };
+
+    cout << f(n);
+
+    return 0;
+}
