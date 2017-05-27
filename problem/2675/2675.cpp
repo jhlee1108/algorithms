a1f96c29529404d033073e282cf9462cf71ea797
jhlee1108 <jeonghyeon.lee1108@gmail.com>
2017-05-27 11:25:30 +0900

    https://www.acmicpc.net/problem/2675

---
+++ problem/2675/2675.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+
+    cin >> t;
+
+    while(t--) {
+        int r;
+        string s;
+
+        cin >> r;
+        cin >> s;
+
+        for(int i=0; i<s.size(); i++)
+            for(int j=0; j<r; j++)
+                cout << s[i];
+        cout << '\n';
+    }
+
+    return 0;
+}
