dd98bf43642e7778afcafb1493dffa866357adb7
jhlee1108 <jeonghyeon.lee1108@gmail.com>
2017-09-28 14:45:33 +0900

    https://www.acmicpc.net/problem/3053

---
+++ problem/3053/3053.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <cmath>
+
+double r;
+
+int main() {
+    scanf("%lf", &r);
+    
+    printf("%.6lf\n", r*r*M_PI);
+    printf("%.6lf\n", r*r*2);
+    
+    return 0;
+}
